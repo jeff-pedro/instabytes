168a0dfb33da14812b1a774de3587e18714c4234
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-12-02 13:41:24 -0300

    corrigindo build da aplicação

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -3,7 +3,7 @@ let API_URL  = process.env.API_URL;
 // Função para buscar os dados do endpoint
 export default async function fetchImages() {
   try {
-    const response = await fetch(process.env.API_URL); // Usando a URL importada
+    const response = await fetch(API_URL); // Usando a URL importada
     const data = await response.json();
     return data;
   } catch (error) {
