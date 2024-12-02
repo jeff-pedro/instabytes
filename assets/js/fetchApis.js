826f8fc295d58dfd77fba0f397ad3a04e0e06cec
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-12-02 14:45:53 -0300

    corrigindo build da aplicação

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -1,16 +1,16 @@
 console.log(process.env.API_URL);
 
-let API_URL = process.env.API_URL;
+// let API_URL = process.env.API_URL;
 
-// Função para buscar os dados do endpoint
-export default async function fetchImages() {
-  try {
-    const response = await fetch(API_URL); // Usando a URL importada
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Erro ao buscar dados:", error);
-  }
-}
+// // Função para buscar os dados do endpoint
+// export default async function fetchImages() {
+//   try {
+//     const response = await fetch(API_URL); // Usando a URL importada
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error("Erro ao buscar dados:", error);
+//   }
+// }
 
 
