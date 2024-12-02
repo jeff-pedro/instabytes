55f9edbc7de3b5c6b709b5b2bfc775318d32d4a8
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-12-02 11:14:12 -0300

    buildando a aplicação

--- dist/index.aaf58ce5.js
+++ dist/index.aaf58ce5.js
@@ -1,4 +1,9 @@
-let{API_URL:e}=process.env;async function t(){try{let e=await fetch(process.env.API_URL);return await e.json()}catch(e){console.error("Erro ao buscar dados:",e)}}const n=document.getElementById("modal"),c=document.getElementById("modal-img"),o=document.getElementById("caption"),r=document.querySelector(".close");n.style.display="none";const i=document.querySelector(".image-grid");async function a(){let e=await t();try{let t=e.map(e=>`
+let{API_URL:e}=process.env;
+
+console.log(API_URL, process.env.NODE_ENV);
+ 
+
+async function t(){try{let e=await fetch(process.env.API_URL);return await e.json()}catch(e){console.error("Erro ao buscar dados:",e)}}const n=document.getElementById("modal"),c=document.getElementById("modal-img"),o=document.getElementById("caption"),r=document.querySelector(".close");n.style.display="none";const i=document.querySelector(".image-grid");async function a(){let e=await t();try{let t=e.map(e=>`
             <article data-description="${e.descricao}">
               <figure>
                 <img src="${e.imgUrl}" alt="${e.alt}" />
